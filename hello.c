353a9a75c2e58b2ecd24d81b5d5c1673d1bff3ef
edlwatson07 <ewatson10@huskers.unl.edu>
2025-08-28 09:38:03 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ed Watson
+ *         ewatson10@unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ed Watson, Computer Engineering\n");
+  return 0;
+}
